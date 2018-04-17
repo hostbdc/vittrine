ae1a9478fca1d639b45c3729cc20b5d581bacb0a
vittrine <central@hostbdc.com.br>
2018-04-17 09:20:13 -0300

    top

--- ortaking-master/www/js/config.js
+++
@@ -1,7 +0,0 @@
-
-var krms_config ={			
-	'ApiUrl' : "http://weborder.chennaiexpress.com/merchantapp/api",	
-	'DialogDefaultTitle' : "Chennai Express Order Taking App",
-	'pushNotificationSenderid' : "507008644414",
-	'APIHasKey' : ""
-};
