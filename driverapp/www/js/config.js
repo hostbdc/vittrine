6620d6d884ecd01a9097068c73d15b6fb8a103b0
vittrine <central@hostbdc.com.br>
2018-04-17 15:54:15 -0300

    4eee

--- driverapp/www/js/config.js
+++ driverapp/www/js/config.js
@@ -1,8 +1,8 @@
 /*Driver App Configuration*/
 
 var krms_driver_config ={		
-	'ApiUrl':"",	
-	'DialogDefaultTitle':"DriverApp",	
-	'PushProjectID':"",	
+	'ApiUrl':"http://deliveryma.com.br/driver/api",	
+	'DialogDefaultTitle':"Entregas MAp",	
+	'PushProjectID':"283086298967",	
 	'APIHasKey':""
 };
