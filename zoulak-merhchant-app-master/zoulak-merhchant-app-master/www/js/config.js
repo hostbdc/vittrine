2ce859e9d4166d93acabb1f72e624abb955e07e6
vittrine <central@hostbdc.com.br>
2018-04-18 13:04:26 -0300

    521

---
+++ zoulak-merhchant-app-master/zoulak-merhchant-app-master/www/js/config.js
@@ -0,0 +1,7 @@
+
+var krms_config ={					
+    'ApiUrl':"http://zoulak.com/merchantapp/api",	           
+	'DialogDefaultTitle':"zoulak merchant app",
+	'pushNotificationSenderid':"600405925358",
+	'APIHasKey':""
+};
